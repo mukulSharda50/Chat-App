539447bbf732dd1a79357eae6a70a2517413448b
Mukul Sharda <mukulsharda5@gmail.com>
2023-01-04 13:58:53 +0530

    Initial commit

---
+++ src/components/HandlePage404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HandlePage404() {
+  return (
+    <div>Page Not Found</div>
+  )
+}
+
+export default HandlePage404
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Login} from './Login';
+export {default as Signup} from './Signup';
+export {default as Chat} from './Chat';
+export {default as HandlePage404} from './HandlePage404';
